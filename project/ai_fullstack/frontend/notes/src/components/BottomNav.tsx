843c24f2faec247824253c04b7c50794d34863c3
chenjunzhang520 <2275732235@qq.com>
2026-02-04 23:23:06 +0800

    项目一

--- project/ai_fullstack/frontend/notes/src/components/BottomNav.tsx
+++ project/ai_fullstack/frontend/notes/src/components/BottomNav.tsx
@@ -1,6 +1,13 @@
 import { Home, User } from 'lucide-react'; // 图标字体库
+import {
+  useNavigate,
+  useLocation
+} from 'react-router-dom';
 
 export default function BottomNav() {
+  const navigate  = useNavigate();
+  const { pathname } = useLocation();
+  // console.log(location, '/////');
   const tabs = [
     {
       label: "首页",
@@ -15,6 +22,10 @@ export default function BottomNav() {
   ]
 
   const handleNav = (path: string) => {
+    if (path === pathname) {
+      return;
+    }
+    navigate(path);
   }
 
   return (
@@ -22,16 +33,20 @@ export default function BottomNav() {
     border-t bg-background flex items-center justify-around
     z-50 safe-area-bottom">
     {
-      tabs.map((tab) => (
-        <button 
+      tabs.map((tab) => {
+        const Icon = tab.icon;
+        return (<button 
           key={tab.path}
           onClick={() => handleNav(tab.path)}
           className="flex flex-col items-center justify-center 
           w-full h-full space-y-1"
         >
-        {tab.label}
+          <Icon size={24} />
+          <span className="text-xs font-medium">
+            {tab.label}
+          </span>
         </button>
-      ))
+      )})
     }
     </div>
   )
