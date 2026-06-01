1a34cafb1132601ef6fc27d5749816baa48dade5
chenjunzhang520 <2275732235@qq.com>
2026-06-01 23:08:51 +0800

    项目实战

--- project/ai_fullstack/readme.md
+++ project/ai_fullstack/readme.md
@@ -43,8 +43,24 @@
   /   Home  延迟一下About 阻止加载
   /About About 
 
-
-
+### 路由有多少种？
+- 普通路由
+- 动态路由 /product/:id
+- 通配路由 * 
+- 嵌套路由 Outlet
+  <Outlet> 是 React Router DOM 中的组件，用于在父路由元素中渲染其子路由匹配到的内容。
+- 鉴权路由(路由守卫) ProtectRoute
+- redirect 重定向路由 Navigate
+### 路由生成访问历史
+  history 栈 先进后出
+  replace redirect 跳转，会替换当前的历史记录 
+### 单页应用
+- 传统的开发是多页的，基于http 请求，每次url 发生改变后，去服务器重新请求整个页面。
+  体验不好，页面会白一下
+- 单页应用 react-router-dom html5 history 
+  前端路由
+  路由改变后
+  前端会收到一个事件，将匹配的新路由显示在页面上
 
 
 
@@ -193,7 +209,7 @@
   api接口文档 
   GET /api/posts?page=1&limit=10 返回内容
   {
-    status: 200,
+    s4吃BEQX 3456789-=00,
     list: Post[]
   }
   只要切换后端真正的地址，无缝对接
--- project/ai_fullstack/router-demo/advance-router/src/App.jsx
+++ project/ai_fullstack/router-demo/advance-router/src/App.jsx
@@ -1,75 +1,16 @@
+
 import {
-  lazy, // 懒加载
-  Suspense //  Suspense 组件，用于包裹懒加载的组件
-} from 'react';
-import {
-  BrowserRouter as Router, // html5 history 
-  // HashRouter,
-  Link,
-  Routes, // 一组路由
-  Route // 单个路由
+  BrowserRouter as Router, // html5 history
 } from 'react-router-dom'
-// import Home from './pages/Home'
-const Home = lazy(() => import('./pages/Home')); // 分享链接
-// import About from './pages/About'
-const About = lazy(() => import('./pages/About'))// 懒加载
-const UserProfile = lazy(() => import('./pages/UserProfile'))
-const Product = lazy(() => import('./pages/product'));
-const ProductDetail = lazy(() => import('./pages/product/ProductDetail'));
-const NewProduct = lazy(() => import('./pages/product/NewProduct'));
-const Login = lazy(()=>import('./pages/Login'));
-const ProtectRoute = lazy(()=>import('./components/ProtectRoute'));
-const Pay = lazy(()=>import('./pages/Pay'));
+import Navigation from './components/Navigation';
+import RouterConfig from './router'
 
 
 export default function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/products">Product</Link>
-          </li>
-          <li>
-            <Link to="/products/new">Product New</Link>
-          </li>
-          <li>
-            <Link to="/products/123">Product Detail</Link>
-          </li>
-          <li>
-            <Link to="/pay">Pay</Link>
-          </li>
-        </ul>
-      </nav>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          {/* 动态路由 http(s)://www.juejin.cn/user/12345?keyword=23#/about
-              协议：// domain/path/:params?queryString
-          */}
-          <Route path="/user/:id" element={<UserProfile />} />
-          <Route path="/products" element={<Product/>}>
-            <Route path=":productId" element={<ProductDetail />}/>
-            <Route path="new" element={<NewProduct />}/>
-          </Route>
-          <Route path="/login" element={<Login/>}/>
-          {/* 鉴权的路由 */}
-          <Route path="/pay" element={
-            <ProtectRoute>
-              <Pay />
-            </ProtectRoute>
-          }>
-
-          </Route>
-        </Routes>
-      </Suspense>
+      <Navigation />
+      <RouterConfig />
     </Router>
   )
 }
