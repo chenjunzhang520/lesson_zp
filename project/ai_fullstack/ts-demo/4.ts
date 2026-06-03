4f3218244b0f7cf641278c1601acccfbe5fed745
chenjunzhang520 <2275732235@qq.com>
2026-06-03 23:06:37 +0800

    项目实战

--- project/ai_fullstack/ts-demo/4.ts
+++ project/ai_fullstack/ts-demo/4.ts
@@ -9,4 +9,58 @@ let user:[number,string] = [1, 'Tom'];
 // 泛型 类型的传参 T
 let arr2: Array<string> = ["a","b"];
 
+// ts 借鉴java  微软 
+// 枚举类型
+enum Status {
+    Pending, // 0
+    Success, // 1
+    Failed, // 2
+}
+// 枚举类型
+let s:Status = Status.Pending;
+s = Status.Success;
+// 类型的推导
+// ts 初学， any 救命
+let aa:any = 1; // 任意类型 救命稻草 放弃类型约束
+aa = "11"
+aa = {}
 
+let bb:unknown = 1;  // 未知类型 更安全一些
+bb = 'b'; // 使用前做类型检测 
+// bb.hello();  // 对象 未知类型，可以接收任何类型，直接调用方法不可以
+
+let user2:{ name: string, age: number, hometown: string } = {
+    name: '陈',
+    age: 18,
+    hometown: '九江',
+}
+// 接口 约定对象 哪些属性和方法
+interface  User {
+    name: string;
+    age: number;
+    readonly id: number;
+    hobby?: string
+}
+
+const u:User = {
+    id: 1,
+    name: '俊',
+    age: 18,
+    hobby: '篮球'
+}
+u.name = "璋";
+u.id = 1111;
+
+type ID = string | number;  // 自定义类型
+let num:ID = "111";
+
+type UserType = {
+    name: string;
+    age: number;
+    hobby?: string
+}
+
+const f: UserType = {
+    name: '陳',
+    age: 111,
+}
