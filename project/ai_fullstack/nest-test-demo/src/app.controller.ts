fc8ddb4ea5956d6c6a59f01adcc1bd06b1dc3a64
chenjunzhang520 <2275732235@qq.com>
2026-06-10 23:05:49 +0800

    项目实战

--- project/ai_fullstack/nest-test-demo/src/app.controller.ts
+++ project/ai_fullstack/nest-test-demo/src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
+import { Controller, Get, Post, Body, Inject } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
   // service 实例
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('PG_CONNECTION') private readonly db: any,
+    private readonly appService: AppService
+  ) {}
 
   @Get()
   getHello() :string {
@@ -17,21 +20,37 @@ export class AppController {
   }
 
   @Post('login')
-  login(@Body() body: {username: string, password: string}){
+  login(@Body() body: { username: string, password: string }) {
     const { username, password } = body;
     console.log(username, password);
-    if(!username || !password){
+    if (!username || !password) {
       return {
         code: 400,
-        message: '用户名或密码不能为空'
+        message: "用户名或密码不能为空"
       }
     }
-    if(password.length < 6){
+    if (password.length < 6) {
       return {
         code: 400,
-        message: '密码长度不能小于6'
+        message: "密码不能少于6位"
       }
     }
     return this.appService.handleLogin(username, password)
+  }
+
+  @Get('db-test')
+  async testConnection() {
+    try {
+      const res = await this.db.query('SELECT * from users');
+      return {
+        status: '连接成功',
+        data: res.rows
+      }
+    } catch(error) {
+      return {
+        status: '连接失败',
+        error: error.message
+      }
+    }
   }
 }
